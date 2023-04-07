0127165c4019bbfb42dda8c95c8e7ffd7e6529f2
Jennifer <jenniferromanmuerte@gmail.com>
2023-04-07 10:41:58 +0200

    Solucionado problema Cors

--- src/main/java/com/help/cook/helpcook/HelpcookApplication.java
+++ src/main/java/com/help/cook/helpcook/HelpcookApplication.java
@@ -2,7 +2,11 @@ package com.help.cook.helpcook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 //@EnableJpaRepositories(basePackages = "com.help.cook.helpcook.repository")
@@ -11,5 +15,14 @@ public class HelpcookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelpcookApplication.class, args);
 	}
-
+	
+@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
