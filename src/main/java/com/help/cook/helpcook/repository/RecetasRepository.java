452939c8c9fc016b4eee0b6358db39b157de3816
Jennifer <jenniferromanmuerte@gmail.com>
2023-04-17 17:41:33 +0200

    Modificado aplication propertyes

--- src/main/java/com/help/cook/helpcook/repository/RecetasRepository.java
+++ src/main/java/com/help/cook/helpcook/repository/RecetasRepository.java
@@ -44,6 +44,6 @@ public interface RecetasRepository extends CrudRepository<Recetas, Integer>{
 	 * Declarada Lista para almacenar las recetas más recientes
 	 * @return Devuelve una Lista de recetas ordenada por las últimas añadidas
 	 */
-	@Query("SELECT r FROM Recetas r ORDER BY fechaAlta DESC")
+	@Query("SELECT r FROM Recetas r ORDER BY fechaAlta")
 	Set<Recetas> findByFechaAlta();
 }
